468f5c82311d40286d5e22f3745044d02bacdae5
Charlie-Warren <c_warren1@live.com>
2024-01-08 12:14:57 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,10 +16,12 @@ fi
 xhost +
 
 # Run Docker container with GUI support
+# May need to change --device paths depending on your system
 docker run -it --name rtab \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --device=/dev/ttyUSB0 \
+    --device=/dev/bus/002/004 \
+    --device=/dev/bus/002/005 \
     humble-rtabmap-pyphase
 
 # Revoke access to X server
