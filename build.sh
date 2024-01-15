1f6f4e32171e24248a8a066722b92174ba41e897
Charlie-Warren <c_warren1@live.com>
2024-01-15 14:34:32 +0000

    update sh scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Check if Docker is installed
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Please install Docker first."
-    exit 1
-fi
-
 docker build -t humble-rtabmap-pyphase
--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Check if Docker is installed
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Please install Docker first."
-    exit 1
-fi
-
-# Check if X server is installed
-if ! command -v X &> /dev/null; then
-    echo "X server is not installed. Please install an X server."
-    exit 1
-fi
-
 # Start X server
 xhost +
 
