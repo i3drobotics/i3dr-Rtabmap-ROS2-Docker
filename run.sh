5a952e3b916e618d5379e104440f4899d6591ad7
Charlie-Warren <c_warren1@live.com>
2024-01-16 12:46:20 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Use lsusb to list USB devices and filter for your camera information
+camera_info=$(lsusb | grep "Basler")
+
+# Extract the bus and device numbers from the lsusb output for each camera
+camera_paths=()
+while read -r line; do
+    bus=$(echo "$line" | awk '{print $2}')
+    device=$(echo "$line" | awk '{print $4}' | tr -d ':')
+    path="/dev/bus/usb/$bus/$device"
+    camera_paths+=("$path")
+done <<< "$camera_info"
+
+# Print the paths
+for ((i=0; i<${#camera_paths[@]}; i++)); do
+    echo "Camera $((i+1)) path: ${camera_paths[i]}"
+done
+
 # Start X server
 xhost +
 
@@ -8,8 +25,8 @@ xhost +
 sudo docker run -it --name rtab \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --device=/dev/bus/002/003 \
-    --device=/dev/bus/002/004 \
+    --device "${camera_paths[0]}" \
+    --device "${camera_paths[1]}" \
     humble-rtabmap-pyphase
 
 # Revoke access to X server
