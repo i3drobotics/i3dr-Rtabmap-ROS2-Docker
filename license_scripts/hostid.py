8eaf732c95f8af8011d67bbdd28966a5bda2954a
Charlie-Warren <c_warren1@live.com>
2024-03-20 12:13:47 +0000

    moved hostid.py and read_lic.py

--- license_scripts/hostid.py
+++ license_scripts/hostid.py
@@ -1,4 +1,5 @@
 def ip_to_hostid(ip:str) -> str:
+    """Converts IP address to host ID"""
     spl = ip.split(".")
     hexstrings = [hex(int(x)) for x in spl]
     trimmed_hexstrings = [hexstring[2:] for hexstring in hexstrings]
@@ -11,6 +12,7 @@ def ip_to_hostid(ip:str) -> str:
     return result[1] + result[0] + result[3] + result[2]
 
 def hostid_to_ip(hostid:str) -> str:
+    """Converts host ID to IP address"""
     intlist = []
     intlist.append(int(hostid[2:4], 16))
     intlist.append(int(hostid[0:2], 16))
@@ -20,11 +22,17 @@ def hostid_to_ip(hostid:str) -> str:
     return ".".join(strlist)
 
 def main():
-    # Example usage
-    print(ip_to_hostid("192.168.11.22"))
-    x = "a8c0160b"
-    print(hostid_to_ip(x))
+    import argparse
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", "-i", type=str, required=True, help="Host ID")
+    args = parser.parse_args()
+
+    hostid = args.id
+    print(hostid_to_ip(hostid))
+
+    # Example usage
+    # python hostid.py -i hostidfromlic
 
 if __name__ == "__main__":
     main()
