22acab116cc67e8b6bcdd96e326a6be7be938a3c
Charlie-Warren <c_warren1@live.com>
2024-03-26 13:58:33 +0000

    made destination folder a variable

--- copy_cal_to_container.sh
+++ copy_cal_to_container.sh
@@ -6,6 +6,9 @@ script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Construct the full path to the calibration data folder
 calibration_data_folder="$script_dir/calibration"
 
+# Define the destination folder path
+destination_folder="frtab:/root/ros2_ws/install/phase_rtabmap_ros2/share/phase_rtabmap_ros2/cal"
+
 # Check if the folder exists
 if [ ! -d "$calibration_data_folder" ]; then
     echo "Error: Calibration data folder not found!"
@@ -13,15 +16,15 @@ if [ ! -d "$calibration_data_folder" ]; then
 fi
 
 echo "Source folder: $calibration_data_folder"
-echo "Destination folder: rtab:/root/ros2_ws/install/phase_rtabmap_ros2/share/phase_rtabmap_ros2/cal"
+echo "Destination folder: $destination_folder"
 
 # Copy the contents of the calibration data folder into the Docker container
-docker cp "$calibration_data_folder/." rtab:/root/ros2_ws/install/phase_rtabmap_ros2/share/phase_rtabmap_ros2/cal
+docker cp "$calibration_data_folder/." "$destination_folder"
 
 # Check if the copy operation was successful
 if [ $? -eq 0 ]; then
-    echo "Calibration data copied successfully into rtab Docker environment."
+    echo "Calibration data copied successfully into frtab Docker environment."
 else
-    echo "Error: Failed to copy calibration data into rtab Docker environment."
+    echo "Error: Failed to copy calibration data into frtab Docker environment."
     exit 1
 fi
